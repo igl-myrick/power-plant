8982cb77ec782bfff14c2dcd5fd0ddaa67d04aa8
igl-myrick <igl.myrick@gmail.com>
2025-10-02 13:58:22 -0700

    add first test for changeState function

---
+++ __tests__/changeState.test.js
@@ -0,0 +1,11 @@
+import { changeState } from '../src/changeState.js';
+
+describe("changeState", () => {
+  const plant = { soil: 0, light: 0, water: 0 };
+  const feed = changeState("soil")(1);
+  const updatedPlant = feed(plant);
+
+  test("should correctly return an object", () => {
+    expect(typeof updatedPlant).toEqual("object");
+  });
+});
