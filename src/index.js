d2bee3ed9a33b8275c4d5b64c88a536a3e646b34
igl-myrick <igl.myrick@gmail.com>
2025-10-08 16:00:49 -0700

    add wip addPlant function

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { storeState } from './storeState.js';
+import { changeState } from './changeState.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+function addPlant(plantName) {
+  if (document.getElementById(`${plantName}-info`)) {
+    return "A plant with this name already exists.";
+  }
+
+  const mainContent = document.getElementById("main-content");
+  const plantWrapper = document.createElement("div");
+
+}
