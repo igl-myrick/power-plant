37df3b08741123d1ae7d78f38469d8b50aa0b817
unknown <igl.myrick@gmail.com>
2025-10-21 16:01:36 -0700

    update addPlant logic

--- src/index.js
+++ src/index.js
@@ -9,10 +9,39 @@ function addPlant(plantName) {
     return "A plant with this name already exists.";
   }
 
+  const snakeCasePlantName = plantName.replace(/\s+/g, '-');
   const mainContent = document.getElementById("main-content");
   const plantWrapper = document.createElement("div");
-  plantWrapper.setAttribute("id", `${plantName}-info`);
+  plantWrapper.setAttribute("id", `${snakeCasePlantName}-info`);
+
   const title = document.createElement("h5");
-  title.textContent = `${plantName}`.replace(/\s+/g, '-');
-  title.setAttribute("id", `${plantName}-description`);
+  title.textContent = `${plantName}`;
+
+  const description = document.createElement("div");
+  description.setAttribute("id", `${snakeCasePlantName}-description`);
+  description.classList.add("row");
+
+  const plantSoilTextWrapper = document.createElement("div");
+  plantSoilTextWrapper.classList.add("col-md-4");
+  const plantSoilText = document.createElement("p");
+  plantSoilText.classList.add("text-center");
+  const plantSoilValue = document.createElement("span");
+  plantSoilValue.setAttribute("id", `${snakeCasePlantName}-soil`);
+  plantSoilValue.innerText = "0";
+
+  const plantLightTextWrapper = document.createElement("div");
+  plantLightTextWrapper.classList.add("col-md-4");
+  const plantLightText = document.createElement("p");
+  plantLightText.classList.add("text-center");
+  const plantLightValue = document.createElement("span");
+  plantLightValue.setAttribute("id", `${snakeCasePlantName}-light`);
+  plantLightValue.innerText = "0";
+
+  const plantWaterTextWrapper = document.createElement("div");
+  plantWaterTextWrapper.classList.add("col-md-4");
+  const plantWaterText = document.createElement("p");
+  plantWaterText.classList.add("text-center");
+  const plantWaterValue = document.createElement("span");
+  plantWaterValue.setAttribute("id", `${snakeCasePlantName}-water`);
+  plantWaterValue.innerText = "0";
 }
